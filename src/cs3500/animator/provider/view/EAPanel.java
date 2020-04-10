b3681872392b46cc754630f21d67e193a6cddfcd
nsimon99 <simon.n@husky.neu.edu>
2020-04-09 23:36:06 -0400

    added providers code

---
+++ src/cs3500/animator/provider/view/EAPanel.java
@@ -0,0 +1,91 @@
+package cs3500.animator.provider.view;
+
+import cs3500.animator.model.ShapeState;
+import cs3500.animator.provider.model.ReadOnlyEAOperations;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.List;
+import javax.swing.JPanel;
+
+/**
+ * Represents a panel that is used to draw the animation. Draws the state of the animation at a
+ * specific time.
+ */
+public class EAPanel extends JPanel {
+
+  private final ReadOnlyEAOperations model;
+  private int tick;
+
+  /**
+   * Constructs an EasyAnimator panel using the given read-only model.
+   *
+   * @param model the model we are reading from
+   * @throws IllegalArgumentException if the given model is null
+   */
+  public EAPanel(ReadOnlyEAOperations model) {
+    super();
+    // check for null, then assign the model.
+    if (model == null) {
+      throw new IllegalArgumentException("Model can't be null");
+    }
+    this.model = model;
+    // start at tick 0
+    this.tick = 0;
+
+  }
+
+  /**
+   * Sets the tick of this panel to the given new tick.
+   *
+   * @param newTick the new tick we want the panel to have
+   */
+  public void setTick(int newTick) {
+    tick = newTick;
+  }
+
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    // check for null
+    if (g == null) {
+      throw new IllegalArgumentException("Graphics can't be null");
+    }
+
+    // always do this!!!
+    super.paintComponent(g);
+
+    // we want 2D graphics, so...
+    Graphics2D g2d = (Graphics2D) g;
+
+    // clip the drawing area to the given width and height
+    g2d.setClip(0, 0, model.getWidth(), model.getHeight());
+
+    // now we paint the thing...
+    List<ShapeState> states = model.getShapeStatesAtTime(tick);
+
+
+    // loop through all states
+    for (ShapeState ss : states) {
+
+      // set the color
+      g2d.setColor(new Color(ss.getRed(), ss.getGreen(), ss.getBlue()));
+
+      // draw each shape, with positions offset by the model's top+left of the canvas
+      switch (ss.getType()) {
+        case RECTANGLE:
+          g2d.fillRect(ss.getX() - model.getLeft(),
+              (ss.getY()) - model.getTop(), ss.getWidth(), ss.getHeight());
+          break;
+        case ELLIPSE:
+          g2d.fillOval(ss.getX() - model.getLeft(), ss.getY() - model.getTop(),
+              ss.getWidth(), ss.getHeight());
+          break;
+        default:
+          return;
+      }
+    }
+  }
+}
+
+
---
+++ src/cs3500/animator/provider/view/IEAEditorView.java
@@ -0,0 +1,186 @@
+package cs3500.animator.provider.view;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.util.List;
+
+/**
+ * Interface that contains all functionality for the editor view for the EasyAnimator animation
+ * studio. Contains methods that can generate all required visual components, which are called by
+ * the controller at the correct times.
+ */
+public interface IEAEditorView extends IEasyAnimatorView {
+
+  /**
+   * Resets the focus of the program, so that the keyboard works.
+   */
+  void resetFocus();
+
+  /**
+   * Generates an info pane withe given message.
+   *
+   * @param message the message we want on the info pane
+   * @param title   the title we want to info pane to have
+   */
+  void generateInfoPane(String message, String title);
+
+  /**
+   * Adds the given key listener to this view.
+   *
+   * @param kl the key listener you want to control the view
+   */
+  void addKeyListener(KeyListener kl);
+
+  /**
+   * Adds the given action listener as a listener for all of the buttons in the animation studio.
+   *
+   * @param listener the listener that will
+   */
+  void addButtonListeners(ActionListener listener);
+
+  /**
+   * Sets the playing label of the animation studio to playing if the given boolean is true, paused
+   * otherwise.
+   *
+   * @param playing represents whether or not the animation is currently playing
+   */
+  void setPlayingLabel(boolean playing);
+
+  /**
+   * Sets the speed label of the animation studio to the given speed value.
+   *
+   * @param speed the speed value we want to set the label to
+   */
+  void setSpeedLabel(int speed);
+
+  /**
+   * Sets the loopback label of the animation studio to "loopback enabled" if the given boolean is
+   * true, "loopback disabled" otherwise.
+   *
+   * @param loopback represents whether or not loopback for the animation is enabled.
+   */
+  void setLoopbackLabel(boolean loopback);
+
+  /**
+   * Generates an add shape pop-up for the animation studio, which allows a user to input the
+   * parameters for adding a new shape.
+   */
+  void generateAddShapePopup();
+
+  /**
+   * Generates the pop-up window for removing a shape in the animation.
+   */
+  void generateRemoveShapePopup();
+
+  /**
+   * Generates the first pop-up window for a user to add a keyframe to an animation. Shows all of
+   * the shapes in the animation to add to as radio buttons in the pop-up window.
+   */
+  void generateAddKeyframePopup1();
+
+  /**
+   * Generates the second pop-up window for a user to add a keyframe to an animation. Shows a text
+   * field which allows the user to input the time they want to add a keyframe at.
+   */
+  void generateAddKeyframePopup2();
+
+  /**
+   * Generates the first pop-up window for removing a keyframe, which contains all of the shapes in
+   * the animation that a user can remove a keyframe from.
+   */
+  void generateRemoveKeyframePopup1();
+
+  /**
+   * Generates the second pop-up window for removing a keyframe, which contains all of the times of
+   * keyframes that can be removed from the shape with the given name.
+   *
+   * @param name the name of the shape that the user selected in the first pop-up
+   */
+  void generateRemoveKeyFramePopup2(String name);
+
+  /**
+   * Generates the correct first pop-up window for editing a keyframe, which has all of the shapes
+   * in the animation that the user can choose to edit.
+   */
+  void generateEditKeyframePopup1();
+
+  /**
+   * Generates the correct second pop-up window for editing a keyframe, which has all of the correct
+   * times of keyframes to choose from using the given shape name.
+   *
+   * @param name the name of the shape that the user selected in the prior pop-up.
+   */
+  void generateEditKeyframePopup2(String name);
+
+  /**
+   * Generates the correct third pop-up window for editing a key frame. Shows 7 text boxes for
+   * editing all of the components of a keyframe.
+   *
+   * @param name the name of the shape the user is editing
+   * @param time the time of the keyframe that the user is editing
+   */
+  void generateEditKeyframePopup3(String name, int time);
+
+  /**
+   * Generates the pop-up window for when a user wants to clear a shape.
+   */
+  void generateClearShapePopup();
+
+  /**
+   * Gets all of the input text which is used to edit a shape state.
+   *
+   * @return the shape state text fields, represented as a list
+   */
+  List<String> getShapeStateTextFields();
+
+  /**
+   * Gets the name of the shape that the user is adding.
+   *
+   * @return the name of the shape the user is adding
+   */
+  String getShapeName();
+
+  /**
+   * Gets the shape type of the shape that the user is adding.
+   *
+   * @return the type of the shape the user is adding
+   */
+  String getShapeType();
+
+  /**
+   * Gets the selected shape name from the list of shape name buttons.
+   *
+   * @return the shape name the user has selected
+   */
+  String getShapeNameFromButtons();
+
+  /**
+   * Gets the time that has been typed into the time text field, as a String.
+   *
+   * @return the time that has been typed into the time text field
+   */
+  String getTimeFromText();
+
+  /**
+   * Gets the correct time button that was selected by the user, or the empty string if no button
+   * was selected.
+   *
+   * @return the time that was clicked in by the user
+   */
+  String getTimeFromButtons();
+
+  /**
+   * Closes the add shape popup of the animation studio..
+   */
+  void closePopup();
+
+  /**
+   * Shows the error message with the given message and title- used if the user input invalid
+   * arguments.
+   *
+   * @param message message of the error pane
+   * @param title   title of the error pane
+   */
+  void error(String message, String title);
+
+}
